98f8a417c95447ad00cf3647fb05afd23136a867
leonardo <apstostrindade1@gmail.com>
2025-09-30 21:29:01 -0300

    user post

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 
-const userRoutes = require('./routes/userRoutes');
-const postRoutes = require('./routes/postRoutes');
+const userRoutes = require('./routes/userRoutes.js');
+const postRoutes = require('./routes/postRoutes.js');
 
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('User route is working');
+    res.send('rota do usuário está funcionando!!!!');
+});
+
+router.post('/', (req, res) => {
+    const { message } = req.body;
+    res.send(`Usuário recebido: ${message}`);
 });
 
 
