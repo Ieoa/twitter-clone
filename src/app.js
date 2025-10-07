33cec2393d9fa10b8295c34d8825fb7137a97e33
leonardo <apstostrindade1@gmail.com>
2025-10-07 17:40:31 -0300

    pequena mudança

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
 
-// 1 Middlewares globais (devem vir antes das rotas)
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-// 2 Importar rotas (antes de usar)
 const userRoutes = require('./routes/userRoutes.js');
 const postRoutes = require('./routes/postRoutes.js');
 
-// 3 Usar as rotas
+
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 
-// 4 Rotas principais
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
@@ -22,5 +20,4 @@ app.get('/feed', (req, res) => {
     res.sendFile(__dirname + '/public/feed.html');
 });
 
-// 5️ Iniciar o servidor
 app.listen(3000, () => console.log('Server rodando em 3000'));
