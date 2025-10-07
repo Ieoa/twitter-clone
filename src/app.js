d7073e644d33ed8bff03e9616c4fa43b89c989d8
leonardo <apstostrindade1@gmail.com>
2025-10-06 21:17:07 -0300

    arrumando rotas e organizando o app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
+
+// 1 Middlewares globais (devem vir antes das rotas)
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+// 2 Importar rotas (antes de usar)
+const userRoutes = require('./routes/userRoutes.js');
+const postRoutes = require('./routes/postRoutes.js');
+
+// 3 Usar as rotas
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+
+// 4 Rotas principais
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 app.get('/feed', (req, res) => {
     res.sendFile(__dirname + '/public/feed.html');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-app.use(express.json());
-app.use(express.static(__dirname + "/public"));
-
-const userRoutes = require('./routes/userRoutes.js');
-const postRoutes = require('./routes/postRoutes.js');
-
-app.use('/users', userRoutes);
-app.use('/posts', postRoutes);
-
+// 5️ Iniciar o servidor
 app.listen(3000, () => console.log('Server rodando em 3000'));
