55a4cb250200706dacd85b25a3db4e03dbdd4a5a
leonardo <apstostrindade1@gmail.com>
2025-10-26 13:53:52 -0300

    styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,20 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Feed from './pages/Feed';
+import Login from './pages/Login';
+import '.styles\App.css';
 
-app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/feed" element={<Feed />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
-const userRoutes = require('./routes/userRoutes.js');
-const postRoutes = require('./routes/postRoutes.js');
+export default App;
 
-
-app.use('/users', userRoutes);
-app.use('/posts', postRoutes);
-
-
-const path = require('path');
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../frontend/src/index.html'));
-});
-
-
-app.get('/feed', (req, res) => {
-  res.sendFile(__dirname + '/public/feed.html');
-});
-
-app.listen(3000, () => console.log('Server rodando em 3000'));
