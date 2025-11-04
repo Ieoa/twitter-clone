3471bbb8e7e1b2bc5d08ff883a56171374f76956
leonardo <apstostrindade1@gmail.com>
2025-11-03 23:02:45 -0300

    tentando arrumar erro do feed

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,14 @@
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
+const feedRoutes = require('./routes/feedRoutes');
+const postRoutes = require('./routes/postRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/feed', feedRoutes);
+app.use('/api/posts', postRoutes);
 
 app.use('/api', userRoutes);
 
